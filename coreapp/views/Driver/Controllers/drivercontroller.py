63a077915898001d25b014bb3cb79a3d088d0d29
vipul-voonik <vipul@voonik.com>
2016-07-25 00:52:20 +0530

    removing csrf for stop trip

--- coreapp/views/Driver/Controllers/drivercontroller.py
+++ coreapp/views/Driver/Controllers/drivercontroller.py
@@ -478,7 +478,7 @@ def get_earning_update(request): # Working here................
 
 	return JsonResponse(res)
 
-# Make this work with stoptrip in android and then make get earning method work
+# @csrf_protect Make this work with stoptrip in android and then make get earning method work
 def get_trip_earning(request):
 	res = {'valid':False}
 	if request.method == 'POST':
