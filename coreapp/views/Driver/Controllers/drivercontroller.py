10bbb89080c8cd7bf1f0fcf3cb4eff625b8c3a86
vipul-voonik <vipul@voonik.com>
2016-07-25 01:09:12 +0530

    removing csrf for stop trip

--- coreapp/views/Driver/Controllers/drivercontroller.py
+++ coreapp/views/Driver/Controllers/drivercontroller.py
@@ -519,6 +519,8 @@ def get_trip_earning(request):
 
 			trip_points = LocationLog.objects.filter(device_uuid=uuid,trip_uuid=tripId)
 
+			print trip_points
+			
 			if len(trip_points)<2:
 				res['error']='Trip to short'
 				return JsonResponse(res)
