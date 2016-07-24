a693a9978fbe91395da290c45f9f56f242ace289
vipul-voonik <vipul@voonik.com>
2016-07-24 23:53:46 +0530

    updated get_trip_earning

--- coreapp/views/Driver/Controllers/drivercontroller.py
+++ coreapp/views/Driver/Controllers/drivercontroller.py
@@ -169,6 +169,7 @@ def get_driver_status(request):
 					print dc
 					if dc!=None and len(dc)!=0:  # check if driver has not signed up for any campaign
 						res['status'] += dc[0].campaign_status
+						print res['campaignId']
 						if dc[0].campaign_status == 2:
 							res['campaignId'] = dc[0].campaign
 						print 'YOLO',res['status']
